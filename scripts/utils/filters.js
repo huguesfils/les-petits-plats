046f57cb513cd43730b8fe4b52073c2f02917b34
Hugues Fils <43337514+huguesfils@users.noreply.github.com>
2022-09-06 21:25:45 +0200

    impove filters function in filters.js

---
+++ scripts/utils/filters.js
@@ -0,0 +1,25 @@
+function applyClickOutToExit(id) {
+  const list = document.getElementById(id + "-list");
+  const btn = document.getElementById(id + "-btn");
+  var isVisible = false;
+  btn.addEventListener("click", () => {
+    list.style.display = "block";
+    btn.style.display = "none";
+    setTimeout(() => {
+      isVisible = true;
+    }, 100);
+  });
+
+  document.addEventListener("click", function (e) {
+    if (list.contains(e.target) == false && isVisible) {
+      console.log("close");
+      list.style.display = "none";
+      btn.style.display = "block";
+      isVisible = false;
+    }
+  });
+}
+
+applyClickOutToExit("ingredient");
+applyClickOutToExit("appliance");
+applyClickOutToExit("ustensils");
