71ec229abbe35f484d36dd4557f0c3effe632dee
Hugues Fils <43337514+huguesfils@users.noreply.github.com>
2022-09-08 21:36:39 +0200

    add item in filters list

--- scripts/factories/filtersFactory.js
+++ scripts/factories/filtersFactory.js
@@ -1,9 +1,23 @@
-export const filtersFactory = (data) => {
-  const { ingredients, appliance, ustensils } = data;
+export const filtersFactory = (filterValue) => {
+  function getFilterListDOM() {
+    //create div for labels
+    const list = document.createElement("div");
+    list.className = "list";
 
+    const labelIngredients = document.createElement("label");
+    labelIngredients.className = "item";
+    for (const ingredient of data.ingredients) {
+      labelIngredients.innerText = filterValue;
+    }
+    list.appendChild(labelIngredients);
+    // console.log(labelIngredients.innerHTML);
+
+    return list;
+  }
   return {
     ingredients,
     appliance,
     ustensils,
+    getFilterListDOM,
   };
 };
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -4,17 +4,50 @@ import { recipesFactory } from "../factories/recipesFactory.js";
 async function displayData(recipes) {
   const recipesSection = document.querySelector(".recipes-content");
   recipesSection.innerHTML = "";
+  const itemList = document.querySelector(".search-filter");
+  // itemList.innerHTML = "";
+
+  var ingredients = new Set();
+  var appliances = new Set();
+  var ustensils = new Set();
 
   recipes.forEach((recipe) => {
-    const dataModel = recipesFactory(recipe);
-    const recipeCardDOM = dataModel.getRecipeCardDOM(recipe);
+    const recipeModel = recipesFactory(recipe);
+    const recipeCardDOM = recipeModel.getRecipeCardDOM(recipe);
     recipesSection.appendChild(recipeCardDOM);
+
+    recipe.ingredients.forEach((ingredient) => {
+      ingredients.add(ingredient.ingredient);
+    });
+    appliances.add(recipe.appliance);
+
+    recipe.ustensils.forEach((ustensil) => {
+      ustensils.add(ustensil);
+    });
   });
+
+  // Filter item qui son pret
+  const list = document.createElement("div");
+  list.className = "list";
+  Array.from(ingredients)
+    .sort()
+    .map((ingredient) => {
+      let label = document.createElement("label");
+      label.innerText = ingredient;
+      return label;
+    })
+    .forEach((div) => list.appendChild(div));
+
+  document.getElementById("ingredient-list").appendChild(list);
 }
 
 async function init() {
   try {
-    displayData(recipes);
+    displayData(
+      recipes.filter((recipe) => {
+        return recipe.time <= 60;
+      })
+    );
   } catch (err) {
     console.log(err);
   }
